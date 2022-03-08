9a17e750c7cc43a736c11158955de82d053cfd37
daisygunn <daisy.l.g@hotmail.co.uk>
2022-03-08 16:39:29 +0000

    Add all_products view, display each product in a card & add pagination

--- products/views.py
+++ products/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from django.views import View
 from .models import Product
 
 
 class AllProducts(View):
-    """ A view to return the index page """
+    """ A view to return the all_products page """
     def get(self, request):
+        """ get request """
         products = Product.objects.all()
+        paginator = Paginator(products, 12)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
         return render(request, 'products/all_products.html',
-                      {'products': products})
+                      {'products': products, 
+                      'page_obj': page_obj})
