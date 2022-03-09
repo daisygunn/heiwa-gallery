23e0a3f56b91c7f1b6a640cb113d415a638a06d4
daisygunn <daisy.l.g@hotmail.co.uk>
2022-03-09 12:22:48 +0000

    Make navbar sticky & make items in footer hidden on smaller screens

---
+++ static/js/base.js
@@ -0,0 +1,38 @@
+$('.toast').toast('show');
+
+function screenClass() {
+    if($(window).innerWidth() < 760) {
+        $(".read-more").removeClass("hidden");
+        $(".read-less").addClass("hidden");
+    } else {
+        $(".read-more").addClass("hidden");
+        $(".read-less").removeClass("hidden");
+    }
+}
+
+// Fire.
+screenClass();
+
+// And recheck when window gets resized.
+$(window).bind('resize',function(){
+    screenClass();
+});
+
+
+// Footer toggle
+function showHours(){
+    $("#opening-hours").toggleClass("hidden");
+}
+
+function showLocation(){
+    $("#location").toggleClass("hidden");
+}
+
+$("#show-opening").on('click', function(event) {
+    showHours();
+})
+
+$("#show-location").on('click', function(event) {
+    showLocation();
+})
+
