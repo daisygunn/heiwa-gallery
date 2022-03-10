c809d4d9118ec2072e451e0fab6f51dec7c234d3
daisygunn <daisy.l.g@hotmail.co.uk>
2022-03-10 11:29:23 +0000

    Update product model to prevent images being uploaded if they already exist in the media folder. Change width of product cards & carousel.

--- products/admin.py
+++ products/admin.py
@@ -4,11 +4,13 @@ from .models import Category, Product
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    """ Category admin management """
     list_display = ("category_name", "name_to_display")
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("pk", "name", "style", "photographer_artist", "size", 
-                    "quantity_in_stock", "price", "image", 
+    """ Product admin management """
+    list_display = ("pk", "name", "style", "photographer_artist", "size",
+                    "quantity_in_stock", "price", "image",
                     "in_stock", "date_added", "style_id")
--- products/models.py
+++ products/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.core.files.storage import FileSystemStorage
 
 
 class Category(models.Model):
     """ category model """
+    class Meta:
+        """ override plural name """
+        verbose_name_plural = 'Categories'
 
     category_name = models.CharField(max_length=50)
     name_to_display = models.CharField(max_length=254, null=True, blank=True)
@@ -24,19 +28,34 @@ product_size_choices = (
 )
 
 
+class MediaFileSystemStorage(FileSystemStorage):
+    """ stop files being renamed if they already exist """
+    def get_available_name(self, name, max_length=None):
+        if self.exists(name):
+            return name
+
+    def _save(self, name, content):
+        if self.exists(name):
+            # if the file exists, do not call the superclasses _save method
+            return name
+        # if the file is new, DO call it
+        return super(MediaFileSystemStorage, self)._save(name, content)
+
+
 class Product(models.Model):
     """ Product model """
     name = models.CharField(max_length=254)
-    style = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True)
+    style = models.ForeignKey(
+        'Category', on_delete=models.SET_NULL, null=True, blank=True)
     photographer_artist = models.CharField(max_length=254, blank=True)
     size = models.CharField(
         max_length=2, choices=product_size_choices, default="A4")
     quantity_in_stock = models.IntegerField(blank=False)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(
+        null=True, blank=True, storage=MediaFileSystemStorage())
     in_stock = models.BooleanField(default=False)
     date_added = models.DateField(auto_now_add=True)
-
 
     def __str__(self):
         """ return name to display on site """
