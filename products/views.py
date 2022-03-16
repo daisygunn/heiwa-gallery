b7b8dcb27806cf8f75548c33922de71929f3ba25
daisygunn <daisy.l.g@hotmail.co.uk>
2022-03-16 08:42:20 +0000

    Fix errors in stock updating view so that in_stock label is changed depending on quantity. Complete USER STORY: STOCK LEVELS CAN BE UPDATED. #17

--- products/views.py
+++ products/views.py
@@ -60,6 +60,7 @@ class AddProduct(View):
 class UpdateProduct(View):
     """ A view to return the update_products page """
     def get(self, request):
+        """ get request, returning all products """
         # IF NOT SUPERUSER
         if not request.user.is_superuser:
             messages.error(request,
@@ -67,13 +68,8 @@ class UpdateProduct(View):
             return redirect(reverse('home'))
         else:
             products = Product.objects.all()
-            # paginate by 12 products
-            paginator = Paginator(products, 12)
-            page_number = request.GET.get('page')
-            page_obj = paginator.get_page(page_number)
             return render(request, 'products/update_products.html',
-                          {'products': products,
-                           'page_obj': page_obj})
+                          {'products': products, })
 
 
 class EditProduct(View):
@@ -88,7 +84,8 @@ class EditProduct(View):
             product = get_object_or_404(Product, pk=pk)
             edit_product_form = EditProductForm(instance=product)
             return render(request, 'products/edit_product.html',
-                          {'edit_product_form': edit_product_form, 'product': product })
+                          {'edit_product_form': edit_product_form,
+                           'product': product})
 
     def post(self, request, pk, *args, **kwargs):
         """ post view """
@@ -103,6 +100,7 @@ class EditProduct(View):
             form = EditProductForm(instance=product)
             messages.success(request, "something went wrong...")
             return redirect(reverse('edit_product'))
+
 
 class DeleteProduct(View):
     """ A view to return the edit_product page """
@@ -124,14 +122,10 @@ class DeleteProduct(View):
         messages.success(
             request, f"success, {product.name} has been deleted.")
         return redirect(reverse('update_products'))
-        # else:
-        #     form = EditProductForm(instance=product)
-        #     messages.success(request, "something went wrong...")
-        #     return redirect(reverse('edit_product'))
 
 
 class StockManagement(View):
-    """ A view to return the all_products page """
+    """ A view to return the stock page """
     def get(self, request, pk):
         """ get request """
         if not request.user.is_superuser:
@@ -150,11 +144,12 @@ class StockManagement(View):
         product = get_object_or_404(Product, pk=pk)
         form = StockForm(request.POST, instance=product)
         if form.is_valid():
-            print('form is valid')
             if form.has_changed():
-                updated_quantity_in_stock = request.POST.get('quantity_in_stock')
+                updated_quantity_in_stock = int(
+                    request.POST.get('quantity_in_stock'))
                 form.save(commit=False)
                 product.quantity_in_stock = updated_quantity_in_stock
+                product.change_stock_label()
                 form.save()
                 messages.success(
                     request, f"success, {product.name} has been updated,\
@@ -162,12 +157,11 @@ class StockManagement(View):
                 return redirect(reverse('update_products'))
             else:
                 messages.warning(
-                    request, f"No updates to {product.name} have been made.")
+                    request, f"You made no changes to {product.name}.")
                 return redirect(reverse('update_products'))
         else:
             form = StockForm(instance=product)
-            print("error")
-            messages.error(request, "something went wrong...")
-            
-        return redirect(reverse('stock', args=[product.pk]))
+            messages.error(request, "something went wrong, stock level"
+                                    " has not been updated...")
 
+        return redirect(reverse('stock', args=[product.pk]))
