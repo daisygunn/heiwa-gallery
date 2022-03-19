050c1e5b40cc9a0b79356e8427d0059a09b2e838
daisygunn <daisy.l.g@hotmail.co.uk>
2022-03-19 08:36:17 +0000

    Add basket breakdown for each time a success message is displayed

--- basket/urls.py
+++ basket/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.BasketOverview.as_view(), name="basket_overview"),
-    path('add_to_bag/<pk>', views.add_product_to_bag, name='add_product_to_bag'),
+    path('add_to_basket/<pk>', views.add_product_to_basket, name='add_product_to_basket'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from products.models import Product
 from django.views import View
 
@@ -12,18 +13,23 @@ class BasketOverview(View):
         return render(request, 'basket/basket.html')
 
 
-def add_product_to_bag(request, pk):
+def add_product_to_basket(request, pk):
     """ add product to bag """
-    # product = get_object_or_404(Product, pk=pk)
+    product = get_object_or_404(Product, pk=pk)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
 
     basket = request.session.get('basket', {})
-
-    if pk in list(basket.keys()):
-        basket[pk] += quantity
+    if product.in_stock:
+        if pk in list(basket.keys()):
+            basket[pk] += quantity
+        else:
+            basket[pk] = quantity
+        messages.success(request, f"{product} has been added to your basket.")
     else:
-        basket[pk] = quantity
+        messages.error(
+            request, f"{product} is not in stock and" 
+            " therefore cannot be added.")
     
     request.session['basket'] = basket
     # import pdb; pdb.set_trace()
