956fd0c25c2ba38553adab03542609215db555e1
daisygunn <daisy.l.g@hotmail.co.uk>
2022-03-19 08:04:40 +0000

    Update basket template to display items in basket

--- basket/context_processors.py
+++ basket/context_processors.py
@@ -1,4 +1,5 @@
-
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def basket_contents(request):
     """ basket context """
@@ -6,11 +7,24 @@ def basket_contents(request):
     items_in_basket = []
     total = 0
     count = 0
+    basket = request.session.get('basket', {})
+
+    for pk, quantity in basket.items():
+        product = get_object_or_404(Product, pk=pk)
+        total += quantity * product.price
+        count += quantity
+        items_in_basket.append({
+            'pk': pk,
+            'quantity': quantity,
+            'product': product,
+        })
 
     context = {
         'items_in_basket': items_in_basket,
         'total_cost': total,
         'product_count': count,
+        'basket': basket,
+        'product': product,
     }
 
     return context
