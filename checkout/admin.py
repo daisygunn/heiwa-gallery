41574ff5b1ee31babe56477da09bae92026f19d9
daisygunn <daisy.l.g@hotmail.co.uk>
2022-03-20 13:33:49 +0000

    Register order model in admin panel

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemAdminInline(admin.TabularInline):
+    """ allows admin to add/edit order items in the admin panel """
+    model = OrderItem
+    readonly_fields = ('orderitem_total',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    """ Order model admin """
+    inlines = (OrderItemAdminInline,)
+
+    readonly_fields = ('order_number', 'order_date', 'order_total',)
+    
+    fields = ('order_number', 'full_name', 'email', 'phone_number', 
+              'flat_house', 'street_address', 'town_city', 'county',
+              'country', 'postcode', 'order_date', 'order_total')
+    
+    list_display = ('order_number', 'order_date', 'full_name',
+                    'order_total',)
+    # displays most recent orders on the top
+    ordering = ('-order_date',)
