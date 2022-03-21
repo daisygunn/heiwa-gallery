d1f2b0bd6713f42d2f05b33e2c5516cfd4637ecb
daisygunn <daisy.l.g@hotmail.co.uk>
2022-03-21 16:39:11 +0000

    Add initial stripe integration in checkout.js to submit card payments

--- checkout/static/checkout/checkout.js
+++ checkout/static/checkout/checkout.js
@@ -41,3 +41,60 @@ const appearance = {
 // Create an instance of the card Element
 let card = elements.create('card', {appearance: appearance});
 card.mount('#payment-element');
+
+card.addEventListener('change', function (event) {
+    if (event.error) {
+        showMessage(`${event.error.message}`)
+    } 
+});
+
+const form = $('#stripe-payment-form')
+
+form.on("submit", function(e){
+    e.preventDefault();
+    setLoading(true);
+    
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            showMessage(`${result.error.message}`)
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                showMessage("Payment succeeded!");
+                setLoading(false);
+                form.submit();
+            }
+    }
+    })
+})
+
+  // ------- UI helpers -------
+
+function showMessage(messageText) {
+    const messageContainer = document.querySelector("#payment-message");
+
+    messageContainer.classList.remove("hidden");
+    messageContainer.textContent = messageText;
+
+    setTimeout(function () {
+        messageContainer.classList.add("hidden");
+        messageText.textContent = "";
+    }, 4000);
+}
+
+// Show a spinner on payment submission
+function setLoading(isLoading) {
+    if (isLoading) {
+        // Disable the button and show a spinner
+        document.querySelector("#submit").disabled = true;
+        document.querySelector("#spinner").classList.remove("hidden");
+        document.querySelector("#button-text").classList.add("hidden");
+    } else {
+        document.querySelector("#submit").disabled = false;
+        document.querySelector("#spinner").classList.add("hidden");
+        document.querySelector("#button-text").classList.remove("hidden");
+}
+}
--- checkout/views.py
+++ checkout/views.py
@@ -38,4 +38,33 @@ class Checkout(View):
         return render(request, 'checkout/checkout.html',
                       {'form': order_form,
                        'stripe_public_key': stripe_public_key,
-                       'client_secret': intent.client_secret,})
+                       'client_secret': intent.client_secret, })
+    def post(self, request):
+        """ post view """
+        order_form = OrderForm()
+        stripe_public_key = settings.STRIPE_PUBLIC_KEY
+        stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+        basket = request.session.get('basket', {})
+        if not basket:
+            messages.error(request, "There isn't currently anything"
+                           " in your basket.")
+            return(redirect('all_products'))
+        
+        current_basket = basket_contents(request)
+        payment_due = current_basket['total_cost']
+        stripe_payment_amount = round(payment_due * 100)
+        stripe.api_key = stripe_secret_key
+        
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_payment_amount,
+            currency='gbp',
+            automatic_payment_methods={
+                'enabled': True,
+            },
+        )
+        messages.success(request, "Order processed")
+        return render(request, 'checkout/checkout.html',
+                      {'form': order_form,
+                       'stripe_public_key': stripe_public_key,
+                       'client_secret': intent.client_secret, })
