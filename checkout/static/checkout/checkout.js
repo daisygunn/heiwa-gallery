52b5cf39d8de6afa8d7c7b4467bf1869adbae885
daisygunn <daisy.l.g@hotmail.co.uk>
2022-03-21 10:09:15 +0000

    Install stripe and add initial payment element to checkout page

---
+++ checkout/static/checkout/checkout.js
@@ -0,0 +1,44 @@
+      // This is your test publishable API key.
+
+const stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+let clientSecret = $('#id_client_secret').text().slice(1, -1);
+let stripe = Stripe(stripePublicKey);
+let elements = stripe.elements();
+const appearance = {
+    theme: 'stripe',
+    variables: {
+        colorPrimary: '#0d5c63',
+        colorText: '#080808',
+        colorBackground: '#ffffff',
+        colorDanger: '#df1b41',
+        fontFamily: 'Ideal Sans, system-ui, sans-serif',
+        spacingUnit: '2px',
+        borderRadius: '4px',
+        
+    },
+    rules: {
+        '.Tab': {
+        border: '1px solid #E0E6EB',
+        boxShadow: '0px 1px 1px rgba(0, 0, 0, 0.03), 0px 3px 6px rgba(18, 42, 66, 0.02)',
+        },
+
+        '.Tab:hover': {
+        color: 'var(--colorText)',
+        },
+
+        '.Tab--selected': {
+        borderColor: '#E0E6EB',
+        boxShadow: '0px 1px 1px rgba(0, 0, 0, 0.03), 0px 3px 6px rgba(18, 42, 66, 0.02), 0 0 0 2px var(--colorPrimary)',
+        },
+
+        '.Input--invalid': {
+        boxShadow: '0 1px 1px 0 rgba(0, 0, 0, 0.07), 0 0 0 2px var(--colorDanger)',
+        },
+
+        // See all supported class names and selector syntax below
+    }
+  };
+
+// Create an instance of the card Element
+let card = elements.create('card', {appearance: appearance});
+card.mount('#payment-element');
--- checkout/views.py
+++ checkout/views.py
@@ -7,5 +7,8 @@ class Checkout(View):
     """ checkout view """
     def get(self, request):
         """ get request """
+        stripe_public_key = 'pk_test_51KUnvPHZrw79J0fC7O6xa3fvr9bSEYSORSfzvtSuM4b3LTvvRecG0SQslEbvfcQEamcTdyb46onuYHnPEw6xywum00frqppeEA'
         order_form = OrderForm()
-        return render(request, 'checkout/checkout.html', {'form': order_form,})
+        return render(request, 'checkout/checkout.html',
+                      {'form': order_form,
+                       'stripe_public_key': stripe_public_key})
