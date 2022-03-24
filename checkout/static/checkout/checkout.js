59e5678073e3bfc0b294c1f425942981eafa6f3d
daisygunn <daisy.l.g@hotmail.co.uk>
2022-03-24 11:45:29 +0000

    Update checkiout template and js to handle form submission so payments are confirmed

--- checkout/static/checkout/checkout.js
+++ checkout/static/checkout/checkout.js
@@ -1,8 +1,8 @@
-      // This is your test publishable API key.
-
 const stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 let clientSecret = $('#id_client_secret').text().slice(1, -1);
-let stripe = Stripe(stripePublicKey, {locale: 'en-GB',});
+let stripe = Stripe(stripePublicKey, {
+    locale: 'en-GB',
+});
 let elements = stripe.elements();
 
 const appearance = {
@@ -13,65 +13,72 @@ const appearance = {
         colorDanger: '#df1b41',
         spacingUnit: '2px',
         borderRadius: '4px',
-        
+
     },
     rules: {
         '.Tab': {
-        border: '1px solid #E0E6EB',
-        boxShadow: '0px 1px 1px rgba(0, 0, 0, 0.03), 0px 3px 6px rgba(18, 42, 66, 0.02)',
+            border: '1px solid #E0E6EB',
+            boxShadow: '0px 1px 1px rgba(0, 0, 0, 0.03), 0px 3px 6px rgba(18, 42, 66, 0.02)',
         },
 
         '.Tab:hover': {
-        color: 'var(--colorText)',
+            color: 'var(--colorText)',
         },
 
         '.Tab--selected': {
-        borderColor: '#E0E6EB',
-        boxShadow: '0px 1px 1px rgba(0, 0, 0, 0.03), 0px 3px 6px rgba(18, 42, 66, 0.02), 0 0 0 2px var(--colorPrimary)',
+            borderColor: '#E0E6EB',
+            boxShadow: '0px 1px 1px rgba(0, 0, 0, 0.03), 0px 3px 6px rgba(18, 42, 66, 0.02), 0 0 0 2px var(--colorPrimary)',
         },
 
         '.Input--invalid': {
-        boxShadow: '0 1px 1px 0 rgba(0, 0, 0, 0.07), 0 0 0 2px var(--colorDanger)',
+            boxShadow: '0 1px 1px 0 rgba(0, 0, 0, 0.07), 0 0 0 2px var(--colorDanger)',
         },
 
         // See all supported class names and selector syntax below
     }
-  };
+};
 
 // Create an instance of the card Element
-let card = elements.create('card', {appearance: appearance});
+let card = elements.create('card', {
+    appearance: appearance
+});
 card.mount('#payment-element');
+
 
 card.addEventListener('change', function (event) {
     if (event.error) {
         showMessage(`${event.error.message}`)
-    } 
+    }
 });
 
-const form = $('#stripe-payment-form')
+var form = document.getElementById('stripe-payment-form');
 
-form.on("submit", function(e){
-    e.preventDefault();
-    setLoading(true);
-    
-    stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-            card: card,
-        }
-    }).then(function(result) {
-        if (result.error) {
-            showMessage(`${result.error.message}`)
-        } else {
-            if (result.paymentIntent.status === 'succeeded') {
-                showMessage("Payment succeeded!");
-                setLoading(false);
-                form.submit();
-            }
+form.addEventListener('submit', function (e) {
+e.preventDefault();
+setLoading(true);
+
+stripe.confirmCardPayment(clientSecret, {
+    payment_method: {
+        card: card,
     }
-    })
+}).then(function (result) {
+    if (result.error) {
+        showMessage(`${result.error.message}`)
+    } else {
+        if (result.paymentIntent.status == 'succeeded') {
+            setLoading(false);
+            showMessage("Payment succeeded!");
+            form.submit();
+
+        }
+    }
+    setLoading(false);
+})
 })
 
-  // ------- UI helpers -------
+
+
+// ------- UI helpers -------
 
 function showMessage(messageText) {
     const messageContainer = document.querySelector("#payment-message");
@@ -89,12 +96,12 @@ function showMessage(messageText) {
 function setLoading(isLoading) {
     if (isLoading) {
         // Disable the button and show a spinner
-        document.querySelector("#submit").disabled = true;
+        document.querySelector("#payment-submit-button").disabled = true;
         document.querySelector("#spinner").classList.remove("hidden");
         document.querySelector("#button-text").classList.add("hidden");
     } else {
-        document.querySelector("#submit").disabled = false;
+        document.querySelector("#payment-submit-button").disabled = false;
         document.querySelector("#spinner").classList.add("hidden");
         document.querySelector("#button-text").classList.remove("hidden");
-}
+    }
 }
