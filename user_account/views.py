37c65304d293ad4ab36604270cf9dd07bb289cdc
daisygunn <daisy.l.g@hotmail.co.uk>
2022-04-05 17:15:02 +0000

    Render userprofile form on template and update view to pre-populate with user instance. Completing USER STORY: FORMS AUTOPOPULATED  #21

--- user_account/views.py
+++ user_account/views.py
@@ -1,17 +1,23 @@
-from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib import messages
-from django.conf import settings
+from .models import UserProfile
+from .forms import UserProfileForm
 
 
-class UserProfile(View):
+class UserProfiles(View):
     """ view to display user profile """
     def get(self, request):
         """ get request """
         if request.user.is_authenticated:
-            user = request.user
+            user = UserProfile.objects.get(user=request.user)
+            info_form = UserProfileForm(instance=user)
+            context = {
+                'form': info_form,
+                # 'user': user,
+            }
             return render(
-                request, 'user_account/user_profile.html')
+                request, 'user_account/user_profile.html', context)
         else:
             messages.warning(
                 request, "You must be logged in to view this page.")
