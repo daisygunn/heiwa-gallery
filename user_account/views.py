129753434d3d3c1a791f28fc5df016e56d28e8cc
daisygunn <daisy.l.g@hotmail.co.uk>
2022-04-05 12:35:23 +0000

    Create initial user profile template/view/url

--- user_account/views.py
+++ user_account/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.views import View
 
-# Create your views here.
+
+class UserProfile(View):
+    """ view to display user profile """
+    def get(self, request):
+        """ get request """
+        return render(
+            request, 'user_account/user_profile.html')
