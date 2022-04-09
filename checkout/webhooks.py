a971144ae0c2486ad444bac5e83772dadfdc3b73
daisygunn <daisy.l.g@hotmail.co.uk>
2022-04-09 11:32:41 +0000

    Fix error to enable webhooks to be handled by stripe

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -16,7 +16,6 @@ def webhook(request):
     payload = request.body
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
-
     try:
         event = stripe.Webhook.construct_event(
             payload, sig_header, wh_secret)
