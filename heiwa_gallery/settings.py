36d55c990956771fc2ca2fd2bc4b53e13500caef
daisygunn <daisy.l.g@hotmail.co.uk>
2022-04-12 11:29:45 +0000

    Install coverage and update settings to use db.sqlite for testing purposes

--- heiwa_gallery/settings.py
+++ heiwa_gallery/settings.py
@@ -128,17 +128,17 @@ WSGI_APPLICATION = 'heiwa_gallery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# if os.environ.get("DEVELOPMENT") == "True":
-#     # Testing database
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
-# else:
-#     # Heroku database
-DATABASES = {
+if os.environ.get("DEVELOPMENT") == "True":
+    # Testing database
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    # Heroku database
+    DATABASES = {
         'default': dj_database_url.parse(os.environ.get("DATABASE_URL")),
         }
 
