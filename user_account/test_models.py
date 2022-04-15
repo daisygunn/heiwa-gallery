4109b027bc39433f88e085158ea886a3d08449cc
daisygunn <daisy.l.g@hotmail.co.uk>
2022-04-15 12:55:56 +0000

    Write model tests for user_account app

---
+++ user_account/test_models.py
@@ -0,0 +1,35 @@
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from .models import UserProfile, UserWishlist
+from products.models import Product, Category
+
+
+class TestExhibitionsViews(TestCase):
+    """ exhibitions app view tests """
+    def setUp(self):
+        self.client = Client()
+        self.category1 = Category.objects.create(
+            category_name='b+w',
+            name_to_display='Black and White'
+        )
+        self.product1 = Product.objects.create(
+            name='Test 1234',
+            style=self.category1,
+            photographer_artist='Tester tester',
+            size='A4',
+            quantity_in_stock=5,
+            price=15.00,
+        )
+        self.user = User(username='project.test@test.com')
+        self.user_profile = UserProfile(
+            user=self.user, email='project.test@test.com')
+
+    def test_user_profile_name(self):
+        user_profile = self.user_profile
+        self.assertEqual(
+            str(user_profile), 'project.test@test.com')
+
+    def test_user_wishlist(self):
+        user_wishlist = UserWishlist(
+            user=self.user_profile, product=self.product1)
+        self.assertEqual(str(user_wishlist), 'Test 1234')
--- user_account/urls.py
+++ user_account/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
      path('', views.account_overview, name="account_overview"),
      path('user_profile/', views.user_profile_display, name="user_profile"),
      path('user_orders/', views.user_orders, name="user_orders"),
-     path('add_to_wishlist/<pk>', views.add_to_wishlist, name="add_to_wishlist"),
+     path('add_to_wishlist/<pk>', views.add_to_wishlist,
+          name="add_to_wishlist"),
      path('wishlist/', views.user_wishlist, name="wishlist"),
-     path('remove_from_wishlist/<pk>', views.remove_from_wishlist, name="remove_from_wishlist"),
+     path('remove_from_wishlist/<pk>', views.remove_from_wishlist,
+          name="remove_from_wishlist"),
 ]
