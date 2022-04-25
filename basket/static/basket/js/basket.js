128ebd30fba918d6502c739cb9e3fc8a8817aa87
daisygunn <daisy.l.g@hotmail.co.uk>
2022-04-25 09:36:35 +0000

    Fix error in basket.js that allowed user to reduce quantity to -1 and add more than 5

--- basket/static/basket/js/basket.js
+++ basket/static/basket/js/basket.js
@@ -1,25 +1,43 @@
 /*jshint esversion: 6 */
-/*globals $:false, */
+/*globals $ */
 
 $(document).ready(function() {
+    // decrease button
     $(".decrease").on('click', function(event){
+        // get data id of button clicked and spit at _ to get the product pk
         var id=$(this).attr('data-id');
         var product = id.split('_');
         var pk = product[0];
+        // get value of the product quantity box using the pk
         var value = parseInt(document.getElementById(pk).value, 10);
         value = isNaN(value) ? 0 : value;
-        value < 1 ? value = 1 : '';
-        value--;
+        // If value is not 0 then reduce by 1
+        if(value != 0){
+            value--;
+        } else {
+            // if value is 0 then alert user they cannot reduce
+            alert("Number must be bigger than zero.");
+            value = value;
+        }
+        // set the quantity box by the new value
         document.getElementById(pk).value = value;
     });
-
+    // increase button
     $(".increase").on('click', function(event){
+         // get data id of button clicked and spit at _ to get the product pk
         var id=$(this).attr('data-id');
         var product = id.split('_');
         var pk = product[0];
+        // get value of the product quantity box using the pk
         var value = parseInt(document.getElementById(pk).value, 10);
         value = isNaN(value) ? 0 : value;
-        value++;
+        if(value <= 4){
+            value++;
+        } else {
+            // if value is bigger than 4 then alert user they buy more than 5
+            alert("You can only purchase a maximum of 5 of a single product");
+            value = value;
+        }
         document.getElementById(pk).value = value;
     });
 
@@ -30,6 +48,7 @@ $(document).ready(function() {
             "paging": false,
             searching: false,
             info: false,
+            // set column priority to hide columns on smaller screen
             columnDefs: [
                 { responsivePriority: 1, targets: 0},
                 { responsivePriority: 10, targets: 1},
